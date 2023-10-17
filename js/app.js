b6438f002b83a1c06c970b6dbd5ce6f749b26cb2
pineapplekk <67015842+pineapplekk@users.noreply.github.com>
2023-10-17 21:00:39 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 // ********** set date ************
 // select span
-const date = (document.getElementById('date').innerHTML =
-  new Date().getFullYear())
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 
 // ********** nav toggle ************
 // select button and links
-const navBtn = document.getElementById('nav-toggle')
-const links = document.getElementById('nav-links')
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
 // add event listener
-navBtn.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
 
 // ********** smooth scroll ************
 // select links
+
+// modified
